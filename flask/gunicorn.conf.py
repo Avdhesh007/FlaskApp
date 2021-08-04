6b3260ab27f8cd7f02c66e517aa23bfb3dadafed
Avdhesh Kumar <avdhesh0076@gmail.com>
2021-08-04 14:24:06 +0530

    2nd

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -1,8 +1,8 @@
-proc_name = "gunicorn_spleeter"
+proc_name = "gunicorn"
 workers = 4
 bind = ['127.0.0.1:5000']
 errolog = "/app/spleeter.gunicorn.error"
-acesslog = "app/spleeter.gunicorn.access"
+acesslog = "/app/spleeter.gunicorn.access"
 loglevel = "debug"
 timeout = 300
 graceful_timeout = 300
